0a466796bb7623aae89a4e3087095eabda66123a
Tamara Savi <terna@bk.ru>
2015-11-25 09:29:03 +0200

    changed back to bk.ru

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,15 @@
 class Contact < MailForm::Base
   attribute :name,       :validate => true
   attribute :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :subject,    :validate =>  true
+  attribute :subject,    :validate => true
+  attribute :deadline,   :validate => true
   attribute :message
   attribute :attach_file
-  attribute :deadline,   :validate => true
 
   def headers
     {
       :subject => %("#{subject}"),
-      :to => "support@inbank.atlassian.net",
+      :to => "terna@bk.ru",
       :from => %("#{name}" <#{email}>)
     }
   end
