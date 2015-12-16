844275a369ffa462c9a8d7af374e50efcce32de0
Tamara Savi <terna@bk.ru>
2015-12-16 11:12:41 +0200

    new logo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < MailForm::Base
   attribute :name,       :validate => true
   attribute :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :subject,    :validate => true
-  attribute :deadline,   :validate => true
+  attribute :telefon   
   attribute :system,     :validate => true
   attribute :info,       :validate => true
   attribute :message
